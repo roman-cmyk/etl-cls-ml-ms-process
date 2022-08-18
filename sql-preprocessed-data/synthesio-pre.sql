51a3087e5db34e642b76268d255299543ae7965d
Roman de la Rosa <fproman393@gmail.ccom>
2022-08-17 21:23:46 -0500

    queries are added to create tables with preprocessed  data: locally

---
+++ sql-preprocessed-data/synthesio-pre.sql
@@ -0,0 +1,9 @@
+BULK INSERT synthesio-pre
+FROM "path\\to\\file.csv"
+WITH
+(
+    FIRSTROW = 2, -- as 1st one is header
+    FIELDTERMINATOR = ',',  --CSV field delimiter
+    ROWTERMINATOR = '\n',   --Use to shift the control to next row
+    TABLOCK
+)
